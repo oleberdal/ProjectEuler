4f92cd9490a3973d9f9588a561f5a90c4c29bfc5
Ole Berdal <oleberdal@gmail.com>
2019-03-11 19:24:50 +0100

    Added solution for problem 27.

--- problems_0-99/problems_20-29/problem_27/quadratic_primes.py
+++ problems_0-99/problems_20-29/problem_27/quadratic_primes.py
@@ -1,7 +1,8 @@
 """
 Author: Berdal, Ole
 Created: 02.03.2019
-Version: Python 3.6.7
+Edited: 11.03.2019
+Version: Python 3.6.8
 
 https://projecteuler.net/problem=27:
 Euler discovered the remarkable quadratic formula:
@@ -23,11 +24,43 @@ import time
 start_time = time.time()
 
 
+def coefficient_product_from_quadratic_prime_expression(absolute_below):
+    prime_cap = abs(absolute_below)
+    primes = list_of_primes(until=prime_cap)
+    maximum_number_of_primes, coefficients = 0, (None, None)
 
+    for a in range(-abs(absolute_below) + 1, abs(absolute_below)):
+        for b in range(3, abs(absolute_below), 2):
+            n, candidate = 1, b
+            while (candidate % 2 or candidate == 2) and candidate > 1 and primes[(candidate - 1) // 2 - 1]:
+                n += 1
+                candidate = n**2 + a * n + b
+
+                while (candidate - 1) / 2 > len(primes):
+                    prime_cap *= 2
+                    primes = list_of_primes(until=prime_cap)
+
+            if n > maximum_number_of_primes:
+                maximum_number_of_primes = n
+                coefficients = (a, b)
+
+    return coefficients[0] * coefficients[-1]
+
+
+def list_of_primes(until):
+    sieve = [True] * ((until - 2) // 2)
+
+    for i in range(len(sieve)):
+        if sieve[i]:
+            prime = 2 * (i + 1) + 1
+            for r in range((prime**2 - 3) // 2, len(sieve), prime):
+                sieve[r] = False
+
+    return sieve
 
 
 def main():
-    solution = unit_fraction_with_longest_recurring_cycle(below=1000)
+    solution = coefficient_product_from_quadratic_prime_expression(absolute_below=1000)
 
     print('Solution: %s.\nExecution time: %s seconds.' % (solution, time.time() - start_time))
 
