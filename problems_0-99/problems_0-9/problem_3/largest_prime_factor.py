b623d555abdd7aceb8d780b1fd65fbd72afa0735
Ole Berdal <oleberdal@gmail.com>
2019-03-27 15:52:40 +0100

    Updated function name.

--- problems_0-99/problems_0-9/problem_3/largest_prime_factor.py
+++ problems_0-99/problems_0-9/problem_3/largest_prime_factor.py
@@ -13,18 +13,18 @@ import time
 start_time = time.time()
 
 
-def find_prime_factors(number, start=3):
+def prime_factors(number, start=3):
     for divisor in range(2 if not number % 2 else start, int(number**0.5) + 1, 1 + number % 2):
         if not number % divisor:
             while not number % divisor:
                 number //= divisor
-            return [divisor] + find_prime_factors(number, divisor + divisor % 2 + 1)
+            return [divisor] + prime_factors(number, divisor + divisor % 2 + 1)
 
     return [number] if number > 1 else []
 
 
 def main():
-    solution = max(find_prime_factors(number=600851475143))
+    solution = max(prime_factors(number=600851475143))
 
     print('Solution: %s.\nExecution time: %s seconds.' % (solution, time.time() - start_time))
 
